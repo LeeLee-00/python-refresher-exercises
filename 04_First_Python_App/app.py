42bb77b24b076774b88ea626a3bf8f74d77619c1
LeeLee-00 <leenoel@live.com>
2023-10-12 14:46:33 -0400

    adding first app - Year conversion

---
+++ 04_First_Python_App/app.py
@@ -0,0 +1,8 @@
+years = int(input("Enter age: "))
+months = years * 12
+weeks = years * 52
+day = years * 365
+hours = years * 8760
+minutes = years * 525600
+seconds = years * 3.154e+7
+print (f"Your age, {years}, is equal to {months} months, and is equal to {weeks} weeks, {day} days, {hours} hours, {minutes} minutes, and finaly {seconds} seconds.")
