e443bb94ed1c886f5ca6728ff34eb5c99f733ccf
LeeLee-00 <leenoel@live.com>
2023-10-13 12:39:22 -0400

    Adding If statement and In Logic practice scripts

---
+++ PracticeScripts/07_If_Statements/code.py
@@ -0,0 +1,10 @@
+day_of_week = input("What day of the week is it today?: ").lower()
+
+if day_of_week == "friday":
+    print("Correct! - Have a great start of your weekend!")
+elif day_of_week == "tuesday":
+    print("Its Tuesday.")
+else:
+    print("Welp figure it out!")
+
+print("This always runs.")
---
+++ PracticeScripts/09_the_in_keyword/code.py
@@ -0,0 +1,4 @@
+moves_watched = {"The Matrix", "Power Rangers", "Get Out"}
+user_movie = input("Enter movie you recenetly watched: ")
+
+print(user_movie in moves_watched)
